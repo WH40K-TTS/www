e0908e533dbe97588a1c5e32c19fe286cabf3f42
WH40K-TTS <117324465+WH40K-TTS@users.noreply.github.com>
2024-11-10 16:01:13 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('imagenes/banner.jpg');
 		
 		// Fixed header
 		//-----------------------------------------------
