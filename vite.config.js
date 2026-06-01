d98badf38d7d7c4ba1a61575f553b39e7de824e7
WH40K-TTS <to@wh40k-tts.com>
2026-06-01 22:06:39 +0200

    www

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pruebas/', // ← ajustar al nombre real del repositorio
+  base: '/www/', // ← ajustar al nombre real del repositorio
 })
